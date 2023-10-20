fed311689606d94e8b240060390f10911ff4cd9a
Pedro Bonaldi <86268880+SenhaDeWiFi@users.noreply.github.com>
2023-10-20 20:19:30 -0300

    boi
    ola

--- components/card/card.js
+++ components/card/card.js
@@ -2,8 +2,16 @@ class Card extends HTMLElement{
 
     connectedCallback(){
         this.innerHTML = /*html*/`
-            <div class="card">
+            <div class="card" draggable="true">
+                
                 <h3>${this.title}</h3>
+
+                <div class="status">
+                
+                </div>
+                <div class="content">
+                
+                </div>
             </div>
         `
         this.style.color = "red"
